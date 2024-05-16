eb981d66e212c7731f969ea9efceeb17d1a5d24a
Avet90 <avet.sarkisian90@icloud.com>
2024-05-16 22:47:54 +0300

    Бе

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -10,20 +10,14 @@ const {
 } = process.env;
 
 module.exports = {
-  apps : [{
-    name: 'mesto-frontend',
-    script: 'npx',
-  }],
-
-  deploy : {
+  deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'rm -rf backend && cd frontend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
-  }
+  },
 };
