cc3f901de558eeebd30dbf2885c4793132b50ce6
Avet90 <avet.sarkisian90@icloud.com>
2024-05-16 18:30:03 +0300

    Деплой

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://ip.mesto.five.nomoredomainswork.ru/');
+  const api = new Api('https://ip.mesto.five.nomoredomainswork.ru');
 
   export default api;
